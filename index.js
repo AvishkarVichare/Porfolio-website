2b8138672e9620b22679e88abcb434233506a63d
Avishkar Vichare <avie9500@gmail.com>
2022-08-04 00:47:11 +0530

    added other sections

--- index.js
+++ index.js
@@ -8,6 +8,45 @@ links.forEach(e=>{
         if(curr){
             curr.classList.remove('nav-active')
         }
+    
         e.classList.add('nav-active')
     })
+})
+
+
+
+
+
+
+// projects 
+
+
+let tabs = document.querySelectorAll('.tab')
+
+tabs.forEach(e=>{
+    e.addEventListener('click',()=>{
+        let tabId = e.getAttribute("data-tab")
+        console.log(tabId)
+
+        let activeTab = document.querySelector('.tab-active')
+        if(activeTab){
+            activeTab.classList.remove('tab-active')
+        }
+
+        let currPro = document.querySelector('.pro-active')
+        // console.log(currPro)
+        if(currPro){
+            currPro.classList.remove('pro-active')
+
+        }
+
+
+        e.classList.add('tab-active')
+
+        document.querySelector(`#${tabId}`).classList.add('pro-active')
+
+    })
+
+
+
 })
